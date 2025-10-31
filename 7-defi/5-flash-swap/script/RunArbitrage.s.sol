e0935e326e3d12d77d6a8df15da1e4f3318df07c
Chen Li <chencassc@gmail.com>
2025-10-31 12:16:00 +0800

    l.

--- 7-defi/5-flash-swap/script/RunArbitrage.s.sol
+++ 7-defi/5-flash-swap/script/RunArbitrage.s.sol
@@ -4,6 +4,11 @@ pragma solidity ^0.8.20;
 import {Script} from "forge-std/Script.sol";
 import {console2} from "forge-std/console2.sol";
 import {FlashSwapArb} from "../src/FlashSwapArb.sol";
+import {IUniswapV2Pair} from "../src/interfaces/IUniswapV2Pair.sol";
+
+interface IERC20 {
+    function balanceOf(address account) external view returns (uint256);
+}
 
 /// @notice Run a flash swap using an already deployed FlashSwapArb contract.
 contract RunArbitrage is Script {
@@ -19,11 +24,17 @@ contract RunArbitrage is Script {
         require(caller != address(0), "CALLER_REQUIRED");
         require(flashArb != address(0), "FLASH_ARB_REQUIRED");
 
+        address profitToken = _resolveProfitToken(pairBorrow, tokenBorrow);
+        uint256 balanceBefore = IERC20(profitToken).balanceOf(profitRecipient);
+
         vm.startBroadcast(caller);
 
         FlashSwapArb(flashArb).startArbitrage(pairBorrow, pairSwap, tokenBorrow, amount, profitRecipient);
 
         vm.stopBroadcast();
+
+        uint256 balanceAfter = IERC20(profitToken).balanceOf(profitRecipient);
+        uint256 profit = balanceAfter > balanceBefore ? balanceAfter - balanceBefore : 0;
 
         console2.log("Arbitrage executed");
         console2.log("Flash contract  :", flashArb);
@@ -32,5 +43,17 @@ contract RunArbitrage is Script {
         console2.log("Token borrowed  :", tokenBorrow);
         console2.log("Borrow amount   :", amount);
         console2.log("Profit recipient:", profitRecipient);
+        console2.log("Profit token    :", profitToken);
+        console2.log("Profit amount   :", profit);
+    }
+
+    function _resolveProfitToken(address pairBorrow, address tokenBorrow) internal view returns (address) {
+        address token0 = IUniswapV2Pair(pairBorrow).token0();
+        address token1 = IUniswapV2Pair(pairBorrow).token1();
+        if (tokenBorrow == token0) {
+            return token1;
+        }
+        require(tokenBorrow == token1, "TOKEN_NOT_IN_PAIR");
+        return token0;
     }
 }
